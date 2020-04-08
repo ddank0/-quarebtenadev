32fd44dcca668d07c49715f2d18f71aa44d935ad
Gabriel <isporck0@gmail.com>
2020-04-08 14:01:39 -0300

    finalizado

--- 01.js
+++ 01.js
@@ -3,9 +3,15 @@ nome = prompt("Digite seu nome?")
 tagh1.innerHTML = ("Nome: ")+ nome
 data = prompt("Data de nascimento : ")
 idade = prompt("Idade:")
-condiçao = ("Tem condição pré-existente ?")
+condiçao = confirm("Tem condição pré-existente ?")
+if (condiçao == true){
+    estado = "Sim"
+}
+else{
+    estado = "Não"
+}
 lista.innerHTML = `
     <li>Data de Nascimento: ${data} </li>
     <li>Idade: ${idade} </li>
-    <li> Tem condição pré-existente: ${condiçao} </li>
+    <li> Tem condição pré-existente: ${estado} </li>
 `
